30372bb1c95c224d16f6f7f68b99aba05c992fe6
pouyasadri <pouyapaghandeh78@gmail.com>
2026-05-26 12:31:48 +0200

    feat: add x-default hreflang support for universal fallback to English version

--- resources/views/components/seo/hreflang.blade.php
+++ resources/views/components/seo/hreflang.blade.php
@@ -5,7 +5,14 @@
     <link rel="alternate" hreflang="{{ $locale }}" href="{{ $url }}">
 @endforeach
 
-{{-- x-default for default language --}}
-@if(isset($hreflang[$defaultLocale]))
+{{--
+    x-default: points to the English version as the universal fallback.
+    Google uses x-default for users whose locale isn't explicitly covered (e.g. Dutch users).
+    Pointing to /fa/ would incorrectly serve Persian to non-Persian speakers.
+    We always use 'en' regardless of $defaultLocale (which is used for internal site logic).
+--}}
+@if(isset($hreflang['en']))
+    <link rel="alternate" hreflang="x-default" href="{{ $hreflang['en'] }}">
+@elseif(isset($hreflang[$defaultLocale]))
     <link rel="alternate" hreflang="x-default" href="{{ $hreflang[$defaultLocale] }}">
 @endif
